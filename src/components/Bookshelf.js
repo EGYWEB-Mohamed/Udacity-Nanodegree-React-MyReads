3651464e540f8ea143ba283310a46185e67df55d
Mohamed Said <personal@msaied.com>
2022-06-07 23:37:48 +0200

    :+1:
    Install router and make components directory

---
+++ src/components/Bookshelf.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Bookshelf extends Component {
+    render() {
+        return (
+            <h1>Shelf</h1>
+        )
+    }
+}
+
+export default Bookshelf
